d6129af2c324fa6c805d4a27998db63bb72cc57f
joseantonio.vasquez <joseantonio.vasquez@serrotconsultores.com>
2023-07-01 16:51:50 -0400

    nueva actualización  page about

--- pages/about/abouts.js
+++ pages/about/abouts.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const abouts = () => {
-  return (
-    <main>
-      <section>
-        <div>
-          <h1>Esta es es la </h1>
-        </div>
-      </section>
-    </main>
-}
+    return (
+        <main>
+            <section>
+                <div>
+                    <div className="cardBody">
+                        <div className="cardTitle">
+                            <h2>Nombre</h2>
+                        </div>
+                        <p>Descripcion del nombre</p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+};
 
-export default abouts
+export default abouts;
