582d0075402600c896f61d17e5870f8e834958ea
joseantonio.vasquez <joseantonio.vasquez@serrotconsultores.com>
2023-07-01 17:46:44 -0400

    nueva seccion de noticias

---
+++ pages/notice/notice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const notice = () => {
+  return (
+	<main>
+		<section>
+			<div>
+				<h1>Seccion de noticias</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro id modi minus rerum nesciunt velit facilis temporibus perferendis, hic explicabo iure suscipit optio omnis esse consequuntur. Reprehenderit illo quae harum.</p>
+			</div>
+		</section>
+	</main>
+  )
+}
+
+export default notice
