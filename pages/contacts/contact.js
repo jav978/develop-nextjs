1e8d3d505c8127e91d64f4560a76419d48977186
joseantonio.vasquez <joseantonio.vasquez@serrotconsultores.com>
2023-07-01 17:04:02 -0400

    nueva seccion de contact

---
+++ pages/contacts/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+	<main>
+		<section>
+			<div>
+				<h1>Page contact</h1>
+			</div>
+		</section>
+	</main>
+  )
+}
+
+export default contact
